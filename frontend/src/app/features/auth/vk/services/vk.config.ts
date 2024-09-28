be4c7f32f5bb3e822551ba833854fd332eef72c4
rukhman <aleksei.rukhmanov@gmail.com>
2024-09-28 16:27:53 +0300

    v

--- frontend/src/app/features/auth/vk/services/vk.config.ts
+++ frontend/src/app/features/auth/vk/services/vk.config.ts
@@ -2,15 +2,16 @@ import * as VKID from '@vkid/sdk';
 import { environment } from '../../../../../environments/environment';
 
 export const initVKConfig = () => {
-  VKID.Config.init({
+  const init = VKID.Config.init({
     app: 52346268, // Идентификатор приложения.
     // redirectUrl: 'https://rukhman.github.io/colabRepo/redirect-auth', // Адрес для перехода после авторизации.
     redirectUrl: environment.redirectUrl, // Адрес для перехода после авторизации.
     state: 'dj29fnsadjsd82', // Произвольная строка состояния приложения.
     codeVerifier: 'FGH767Gd65', // Верификатор в виде случайной строки. Обеспечивает защиту передаваемых данных.
     scope: 'email phone', // Список прав доступа, которые нужны приложению.
-    // mode: VKID.ConfigAuthMode.InNewTab, // По умолчанию авторизация открывается в новой вкладке.
-    mode: VKID.ConfigAuthMode.Redirect, // По умолчанию авторизация открывается в новой вкладке.
+    mode: VKID.ConfigAuthMode.InNewTab, // По умолчанию авторизация открывается в новой вкладке.
+    // mode: VKID.ConfigAuthMode.Redirect, // По умолчанию авторизация открывается в новой вкладке.
     __debug: true,
   });
+  console.log(init);
 };
