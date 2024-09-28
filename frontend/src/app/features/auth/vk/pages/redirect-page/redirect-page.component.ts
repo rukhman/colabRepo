33b6098a2cf58f4b5e26e6e484d910cbd18de7f0
rukhman <aleksei.rukhmanov@gmail.com>
2024-09-28 16:07:45 +0300

    d

--- frontend/src/app/features/auth/vk/pages/redirect-page/redirect-page.component.ts
+++ frontend/src/app/features/auth/vk/pages/redirect-page/redirect-page.component.ts
@@ -14,6 +14,9 @@ export class RedirectPageComponent implements OnInit {
   constructor(private route: ActivatedRoute) {}
   ngOnInit(): void {
     console.log(this.route.snapshot.queryParams);
+    console.log(this.route.snapshot.queryParams['code']);
+    console.log(this.route.snapshot.queryParams['deviceId']);
+
     initVKConfig();
     VKID.Auth.exchangeCode(
       this.route.snapshot.queryParams['code'],
