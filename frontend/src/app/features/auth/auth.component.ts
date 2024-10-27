ca6496a26242a9a22fdffc15ac2eed6726edeee9
rukhman <aleksei.rukhmanov@gmail.com>
2024-10-27 20:27:42 +0300

    3

--- frontend/src/app/features/auth/auth.component.ts
+++ frontend/src/app/features/auth/auth.component.ts
@@ -28,17 +28,17 @@ export class AuthComponent {
       fullName: 'ВКонтакте',
       path: 'assets/icons/vk.svg',
       handler: () => {
-        VKID.Config.init({
-          app: 52346268, // Идентификатор приложения.
-          // redirectUrl: 'https://rukhman.github.io/colabRepo/redirect-auth', // Адрес для перехода после авторизации.
-          redirectUrl: 'http://localhost:4200/redirect-auth/', // Адрес для перехода после авторизации.
-          state: 'dj29fnsadjsd82', // Произвольная строка состояния приложения.
-          codeVerifier: 'FGH767Gd65', // Верификатор в виде случайной строки. Обеспечивает защиту передаваемых данных.
-          scope: 'email phone', // Список прав доступа, которые нужны приложению.
-          mode: VKID.ConfigAuthMode.InNewTab, // По умолчанию авторизация открывается в новой вкладке.
-          // mode: VKID.ConfigAuthMode.Redirect, // По умолчанию авторизация открывается в новой вкладке.
-          __debug: true,
-        });
+        // VKID.Config.init({
+        //   app: 52346268, // Идентификатор приложения.
+        //   // redirectUrl: 'https://rukhman.github.io/colabRepo/redirect-auth', // Адрес для перехода после авторизации.
+        //   redirectUrl: 'http://localhost:4200/redirect-auth/', // Адрес для перехода после авторизации.
+        //   state: 'dj29fnsadjsd82', // Произвольная строка состояния приложения.
+        //   codeVerifier: 'FGH767Gd65', // Верификатор в виде случайной строки. Обеспечивает защиту передаваемых данных.
+        //   scope: 'email phone', // Список прав доступа, которые нужны приложению.
+        //   mode: VKID.ConfigAuthMode.InNewTab, // По умолчанию авторизация открывается в новой вкладке.
+        //   // mode: VKID.ConfigAuthMode.Redirect, // По умолчанию авторизация открывается в новой вкладке.
+        //   __debug: true,
+        // });
         VKID.Auth.login();
       },
     },
